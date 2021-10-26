755b27b5e0dcc4b57cbc50ec4d1f232b8c6ff3e6
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-10-26 15:45:06 +0000

    feat: add validation on form input

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,16 +5,25 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef(null)
+  const [error, setError] = React.useState(null)
+
   function handleSubmit(event) {
     event.preventDefault()
     onSubmitUsername(inputRef.current.value)
+  }
+
+  function handleChange(event) {
+    const value = event.target.value
+    const isValid = value === value.toLowerCase()
+    setError(isValid ? null : 'Username must be lower case')
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" ref={inputRef} type="text" />
+        <input id="usernameInput" ref={inputRef} type="text" onChange={handleChange} />
+        <span role="alert" style={{color: '#cf222e'}}>{error}</span>
       </div>
       <button type="submit">Submit</button>
     </form>
