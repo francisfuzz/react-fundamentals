c85d9e675e289e29adf20e91c34ea1cfcf84a4e6
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-10-26 16:01:22 +0000

    refactor: update handleChange to lowercase input rather than showing error

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,28 +5,37 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef(null)
-  const [error, setError] = React.useState(null)
 
+  // Create a new State Hook for the username.
+  const [username, setUsername] = React.useState('')
+
+  // On submission, prevent the default behavior
+  // and call the onSubmitUsername callback with the username.
   function handleSubmit(event) {
     event.preventDefault()
     const { value } = inputRef.current
     onSubmitUsername(value)
   }
 
+  // When the input changes, immediately lowercase the username.
   function handleChange(event) {
     const { value } = event.target
-    const isLowerCase = value === value.toLowerCase()
-    setError(isLowerCase ? null : 'Username must be lower case')
+    setUsername(value.toLowerCase())
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" ref={inputRef} type="text" onChange={handleChange} />
-        <span role="alert" style={{color: '#cf222e'}}>{error}</span>
+        <input
+          id="usernameInput"
+          ref={inputRef}
+          type="text"
+          value={username}
+          onChange={handleChange}
+        />
       </div>
-      <button disabled={Boolean(error)} type="submit">
+      <button disabled={!Boolean(username)} type="submit">
         Submit
       </button>
     </form>
