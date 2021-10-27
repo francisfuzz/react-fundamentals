7f8623de98c11bc80d15dd4eb0e80a14719b70eb
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-10-27 22:19:20 +0000

    final: put all the pieces together!

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,60 +1,45 @@
 // Styling
-// http://localhost:3000/isolated/exercise/05.js
+// 💯 accept a size prop to encapsulate styling
+// http://localhost:3000/isolated/final/05.extra-2.js
 
 import * as React from 'react'
 import '../box-styles.css'
+import Box from './Box'
+import shuffle from './shuffle'
 
-// Set up the Box interface so a size, a classname, styles,
-// and any other props can be passed to the Box component.
-function Box ({size, className = '', style = {}, ...props}) {
-  // Map for available box sizes
-  const sizeClasses = {
-    small: 'box--small',
-    medium: 'box--medium',
-    large: 'box--large',
-  }
-
-  // Try to set box size based on client's choice.
-  // If the client's choice isn't available, don't set a size.
-  const sizeClass = sizeClasses[size] || ''
-
-  return (
-    <div
-      {...props}
-      className={['box', sizeClass, className].filter(Boolean).join(' ')}
-      style={{fontStyle: 'italic', ...style}}
-    />
-  )
-}
-
-
-const smallBox
-  = <Box
-      size="small"
-      style={{ backgroundColor: 'lightblue' }}>
-      small lightblue box
-    </Box>
-
-const mediumBox
-  = <Box
-      size="medium"
-      style={{ backgroundColor: 'pink' }}>
-      medium pink box
-    </Box>
-
-const largeBox
-  = <Box
-      size="large"
-      style={{ backgroundColor: 'orange' }}>
-      large orange box
-    </Box>
+// Source: https://primer.style/primitives/colors#scale-variables
+const GREEN_SCALE = [
+  '#dafbe1',
+  '#aceebb',
+  '#6fdd8b',
+  '#4ac26b',
+  '#2da44e',
+  '#1a7f37',
+  '#116329',
+  '#044f1e',
+  '#003d16',
+  '#002d11'
+]
 
 function App() {
+  const [colors, setColors] = React.useState(GREEN_SCALE)
+
+  React.useEffect(() => {
+    const id = setInterval(() => setColors(shuffle), 100)
+    return () => clearInterval(id)
+  })
+
+  // const uniqueKey = () => Math.random().toString(36).slice(2)
+
   return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+    <div class="container">
+      {colors.map(color => (
+        <Box 
+          size="small"
+          key={color}
+          style={{backgroundColor: color}}>
+        </Box>
+      ))}
     </div>
   )
 }
